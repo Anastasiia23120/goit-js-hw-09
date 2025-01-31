eae00ce410937e6ee370582926e96c61dcbc57ac
Anastasiia Medved <nastya0662793393@gmail.com>
2025-01-31 05:23:02 +0100

    hw

---
+++ src/js/2-form.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+
+input.placeholder = '';
+textArea.placeholder = '';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('input', () => {
+  formData.email = input.value;
+  formData.message = textArea.value;
+
+  try {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  console.log(formData);
+  input.value = '';
+  textArea.value = '';
+  formData.email = '';
+  formData.message = '';
+  localStorage.clear();
+});
+
+const getValue = () => {
+  try {
+    const formLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (formLS) {
+      if (formLS.email) {
+        input.value = formLS.email;
+        formData.email = formLS.email;
+      }
+      if (formLS.message) {
+        textArea.value = formLS.message;
+        formData.message = formLS.message;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getValue();
